8b58d9c91dfdb9dc51b21eeaf44745264f46fbe4
divyanshu <dshapbss202@gmail.com>
2025-03-19 03:15:04 +0530

    onboarding user register and avatar component

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 import Image from "next/image";
 import { useStateProvider } from "@/context/StateContext";
+import Input from "@/components/common/Input";
+import Avatar from "@/components/common/Avatar";
 function onboarding() {
   const [{userInfo}] = useStateProvider()
-  return <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
+  const [name,setName] = useState(userInfo?.name || "")
+  const [about, setAbout] = useState("")
+  const [image, setImage] = useState("/default_avatar.png")
+  return <div className="bg-panel-header-background min-h-screen min-w-screen text-white flex flex-col items-center justify-center">
     <div className="flex items-center justify-center gap-2">
       <Image
       src="/whatsapp.gif"
@@ -15,8 +20,12 @@ function onboarding() {
     </div>
     <h2 className="text-2xl">Create Your Profile</h2>
     <div className="flex mt-6 gap-6">
-      <div className="flex flexx-col items-center justify-center mt-5 gap-6">
-          {userInfo.name}
+      <div className="flex flex-col items-center justify-center mt-5 gap-6">
+          <Input name="Display Name" state={name} setState={setName} label/>
+          <Input name="About" state={about} setState={setAbout} label/>
+      </div>
+      <div>
+        <Avatar type="xl" image={image} setImage={setImage}/>
       </div>
 
     </div>
