8d12fd385becfbee2391501308e006f46ce393d1
divyanshu <dshapbss202@gmail.com>
2025-04-05 18:06:19 +0530

    upload photo done

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,19 +1,63 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import Image from "next/image";
 import {FaCamera} from "react-icons/fa"
+import ContextMenu from "./ContextMenu";
+import PhotoPicker from "./PhotoPicker";
 
 function Avatar({ type, image, setImage }) {
   const [hover, setHover] = useState(false)
   const [isContextMenuVisible, setIsContextMenuVisible] = useState(false)
+  const [grabPhoto, setGrabPhoto] = useState(false)
   const [contextMenuCoordinates, setContextMenuCoordinates] = useState({
     x:0,
     y:0,
   })
 
+  useEffect(() => {
+    if(grabPhoto){
+      const data = document.getElementById("photo-picker")
+      data.click()
+      document.body.onfocus = (e) => {
+        setTimeout(() => {
+          setGrabPhoto(false)
+        }, 1000)
+      }
+    }
+  },[grabPhoto])
+
   const showContextMenu = (e) => {
-    e.preventDefault();
-    setIsContextMenuVisible(true)
+    e.preventDefault( )
     setContextMenuCoordinates({x:e.pageX,y:e.pageY})
+    setIsContextMenuVisible(true)
+  }
+
+
+
+  const contextMenuOptions = [
+    {name:"Take Photo",callback:()=>{}},
+    {name:"Upload From Liabrary",callback:()=>{}},
+    {name:"Upload Photo",callback:()=>{
+      setGrabPhoto(true)
+    }},
+    {name:"Remove Photo",callback:()=>{
+      setImage("default_avatar.png")
+    }},
+  ]
+
+  const photoPickerChange = async (e) => {
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    console.log("file",{file})
+    const data = document.createElement("img")
+    reader.onload=function(event){
+      data.src = event.target.result
+      data.setAttribute("data-src",event.target.result)
+    }
+    reader.readAsDataURL(file)
+    setTimeout(()=>{
+      console.log("image base64Url",data.src)
+      setImage(data.src)
+    },100)
   }
   return (
     <>
@@ -52,6 +96,12 @@ function Avatar({ type, image, setImage }) {
           )
         }
       </div>
+      {
+        isContextMenuVisible && <ContextMenu options={contextMenuOptions} coordinates={contextMenuCoordinates} contextMenu={isContextMenuVisible} setContextMenu={setIsContextMenuVisible}/>
+      }
+      {
+        grabPhoto && <PhotoPicker onChange={photoPickerChange}/>
+      }
     </>
   );
 }
--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,22 +1,44 @@
-import React, { useRef } from "react";
+import React, { useRef,useEffect } from "react";
 
-function ContextMenu({options,coordinates,contextMenu,setContextMenu}) {
-  const contextMenuRef = useRef(null)
-  const handleClick = (e,callback) => {
-    
-  }
+function ContextMenu({ options, coordinates, contextMenu, setContextMenu }) {
+  const contextMenuRef = useRef(null);
+  const handleClick = (e, callback) => {
+    e.preventDefault()  
+    callback();
+    setContextMenu(false)
+  };
 
-  return <div className={`bg-dropdown-background fixed py-2 z-[100] top-[${coordinates.y}] left-[${coordinates.x}] shadow-xl`} ref={contextMenuRef}>
-    <ul>
-      {
-        options.map(({name,callback})=>(
-          <li key={name} onClick={()=>handleClick(e,callback)}>
-            <span className="text-white">{name}</span>
-            </li>
-        ))
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if(e.target.id !== "context-opener") {
+        if (contextMenuRef.current && !contextMenuRef.current.contains(e.target)) {
+          setContextMenu(false);
+        }
       }
-    </ul>
-  </div>;
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }
+  ,[]);
+
+  return (
+    <div
+      className={`bg-dropdown-background fixed py-2 z-[100] shadow-xl`}
+      ref={contextMenuRef}
+      style={{
+        top: coordinates.y,
+        left: coordinates.x,
+      }}
+    >
+      <ul>
+        {options.map(({ name, callback }) => (
+          <li key={name} onClick={(e) => handleClick(e, callback)} className="px-5 py-2 cursor-pointer hover:bg-background-default-hover">
+            <span className="text-white">{name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ContextMenu;
